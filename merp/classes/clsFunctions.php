3b61a5bba1b7aa3f721b75751a975d14b0cab189
Stijn Spruit <168417@student.horizoncollege.nl>
2023-10-03 16:29:30 +0200

    functions will be the place where most helper functions will come.

---
+++ merp/classes/clsFunctions.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace classes;
+
+class clsFunctions
+{
+//TODO: move the helper functions into this file
+//TODO: make a function that can set your code mode to clsCodeMode::DEBUG or clsCodeMode::NORMAL, with an enum containing those values.
+}
